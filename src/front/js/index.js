68c6549a7443906d750145c01ff4a44e9fa19fb0
Eduardo Loreto <138321528+EduardoLoreto@users.noreply.github.com>
2024-07-17 17:22:09 +0000

    Fronted Montado

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -1,15 +1,11 @@
 //import react into the bundle
 import React from 'react'
-import {createRoot} from 'react-dom/client'
+import ReactDOM from 'react-dom'
 
 //include your index.scss file into the bundle
 import "../styles/index.css";
 
 //import your own components
-import Layout from './Layout'
+import Layout from './Layout.js'
 
-//
-const root = createRoot(document.querySelector("#app"))
-
-//render your react application
-root.render(<Layout/>)
+ReactDOM.render(<Layout/>, document.querySelector("#app"))
