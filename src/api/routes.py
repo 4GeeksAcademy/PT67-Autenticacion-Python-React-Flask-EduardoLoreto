3c101ca77b876ad65db640806121a1b9d3c60d78
Eduardo Loreto <138321528+EduardoLoreto@users.noreply.github.com>
2024-07-31 15:52:37 +0000

    Creacion del Login, JWT

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,29 +1,44 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 from flask import Flask, request, jsonify, url_for, Blueprint
-from models import db, User, People, Planets, Vehicles, UserFavorites
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from api.models import db, User, People, Planets, Vehicles, UserFavorites
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
 api = Blueprint('api', __name__)
 
-# Allow CORS requests to this API
 CORS(api)
 
+#  Login, Access Token, Protected y JWT Requiered ----------------------------------------------------
+
+@api.route('/login', methods=['POST'])
+def login():
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+    # Aquí deberías validar contra tu base de datos
+    users_query = User.query.filter_by(email=email, password=password).first()
+    if email != users_query.email or password != users_query.password:
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token), 200
+
+@api.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user()), 200
 
 @api.route('/hello', methods=['POST', 'GET'])
+@jwt_required()
 def handle_hello():
-
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
-
-    return jsonify(response_body), 200
-
+    return jsonify(response_body), 200 
 
 
-    # USER-----------------------------------------------------------------------------------------------
+    
+# USER-----------------------------------------------------------------------------------------------
 
 #Create a User
 @api.route('/user', methods=['POST'])
@@ -353,4 +368,4 @@ def delete_vehicle_fav(vehicle_id):
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+    api.run(host='0.0.0.0', port=PORT, debug=False)
